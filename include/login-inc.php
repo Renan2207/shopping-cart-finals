4d30e9d323a83dd473ba58927d0df39b198441d1
Renan Angeles <Rennan.angeles22@gmail.com>
2022-12-10 16:39:48 +0800

    Backend-login-inc

---
+++ include/login-inc.php
@@ -0,0 +1,34 @@
+<?php 
+	if (isset($_POST['submitBTN'])) {
+		session_start();
+		require 'database.php';
+
+		$emailAddress = $_POST['username'];
+		$password = $_POST['password'];
+	
+		$sql = "SELECT * FROM tbl_user";
+		$stmt = $conn->prepare($sql);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if (!empty($result)) {	
+			while ($row = $result->fetch_assoc()) {
+				$inputHashPass = md5($password);
+
+				$emailMatch = ($username == $row['username']) ? true : false;
+				$passMatch = ($inputHashPass == $row['password']) ? true : false;
+				if ($userMatch == true && $passMatch == true) {
+					$_SESSION['name'] = $row['firstName'] . " " . $row['lastName'];
+					$_SESSION['customerID'] = $row['customerID'];
+					header("Location: ../index.php");
+					exit();		
+				} else {
+					$_SESSION['message'] = "Error = Incorrect Credentials".$tempName.$passMatch.$terminate;
+					$_SESSION['messageType'] = "danger";
+					header("Location: ../login.php");
+				}
+			}					
+			$stmt->close();
+			$conn->close();
+		}		
+	}
